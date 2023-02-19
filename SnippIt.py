c6630887e950ce23529d3bf9bee38487c9240ee5
Justin Soberano <jsoberano2002@gmail.com>
2023-02-19 00:40:00 -0600

    Added

---
+++ SnippIt.py
@@ -0,0 +1,81 @@
+from googleapiclient.discovery import build
+from pytube import YouTube
+from pydub import AudioSegment
+import os
+
+
+API_KEY = "Use Own API key"    
+    
+def urlCreator():
+    print("Welcome to SnippIt: Song Snippet Maker")
+    songName = input("Song name: ")
+    artistName = input("Artist name: ")
+    
+    # Creates the search query used for the YouTube API
+    searchQuery = songName + " " + artistName + " audio"
+    
+    # Creates the YouTube API
+    youtube = build(
+        'youtube',
+        'v3',
+        developerKey = API_KEY
+    )
+    
+    # Make request to the YouTube API
+    request = youtube.search().list(
+        part = 'snippet',
+        maxResults = 1,
+        q = searchQuery,
+    )
+    
+    response = request.execute()
+    
+    #Grabs the video ID from the JSON response, needed for the next request
+    videoID = response['items'][0]['id']['videoId']
+
+    url = "https://www.youtube.com/watch?v=" + videoID
+    return url
+
+def mp4Downloader():
+    url = urlCreator()
+    video_object = YouTube(url)
+    base_dir = downloadFolder()
+
+    # Grabs highest quality of audio available and defaults to mp4
+    song = video_object.streams.get_audio_only()
+    
+    try:
+        song.download(downloadFolder())
+        print(song.title + " successfully loaded.")
+        sound = AudioSegment.from_file(os.path.join(base_dir, song.title + ".mp4"))
+        
+        beginTime = input("Where would you like the song snippet to begin? (mm:ss) || ")
+        EndTime = input("Where would you like the song snippet to end? (mm:ss) || ")
+    
+        startTime = beginTime.split(":")
+        EndTime = EndTime.split(":")
+    
+        start = ((60 * int(startTime[0])) + int(startTime[1])) * 1000
+        end = ((60 * int(EndTime[0])) + int(EndTime[1])) * 1000
+        
+        clip = sound[start:end]
+        
+        clip.export(os.path.join(base_dir, song.title + ".mp4"), format="mp4")
+        
+        print("A snippet of " + song.title + " has been downloaded to your downloads folder")
+        
+    except:
+        print("Try Again: Error downloading file")
+       
+def downloadFolder():
+    # Checks the OS and sets the download folder accordingly
+    # Windows
+    if os.name == "nt":
+        directory = f"{os.getenv('USERPROFILE')}\\Downloads"
+    # Unix Based
+    else: 
+        directory = f"{os.getenv('HOME')}/Downloads"
+
+    return directory
+
+mp4Downloader()
